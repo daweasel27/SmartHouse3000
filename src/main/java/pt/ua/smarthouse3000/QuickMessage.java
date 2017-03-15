9ce595eaec5d3c221b89109f7b2c76d3871d847f
daweasel27 <joaopaulomelo@ua.pt>
2017-03-15 00:04:19 +0000

    Created MAVEN project and SQLITE Db

---
+++ src/main/java/pt/ua/smarthouse3000/QuickMessage.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.ua.smarthouse3000;
+
+/**
+ *
+ * @author Jota
+ */
+public class QuickMessage {
+    
+}
